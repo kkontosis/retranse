8f1073d3232dc0ccc5fdbc1c42a9dd70319977cb
Kimon Kontosis <14114238+kkontosis@users.noreply.github.com>
2013-02-21 00:13:00 +0000

    changed 2012 to 2013

--- retranse_run.cpp
+++ retranse_run.cpp
@@ -1,5 +1,5 @@
 /*  libretranse, a regular expression based programming language library 
- *  Copyright (C) 2012, Kimon Kontosis
+ *  Copyright (C) 2013, Kimon Kontosis
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published
